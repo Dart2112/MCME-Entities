d8f9840f8e6b3c7785fdde09660634766a84cf05
opl- <opl-@users.noreply.github.com>
2022-07-21 03:14:17 +0200

    Remove unused imports
    graalvm specifically doesn't exist on some JDKs.

--- src/main/java/com/mcmiddleearth/entities/entities/simple/SimpleEntity.java
+++ src/main/java/com/mcmiddleearth/entities/entities/simple/SimpleEntity.java
@@ -6,14 +6,13 @@ import com.mcmiddleearth.entities.api.VirtualEntityFactory;
 import com.mcmiddleearth.entities.entities.VirtualEntity;
 import com.mcmiddleearth.entities.exception.InvalidDataException;
 import com.mcmiddleearth.entities.exception.InvalidLocationException;
-import com.mcmiddleearth.entities.protocol.packets.*;
+import com.mcmiddleearth.entities.protocol.packets.AbstractPacket;
+import com.mcmiddleearth.entities.protocol.packets.DisplayNamePacket;
+import com.mcmiddleearth.entities.protocol.packets.VirtualEntityDestroyPacket;
 import com.mcmiddleearth.entities.protocol.packets.simple.*;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.EquipmentSlot;
 import org.bukkit.inventory.ItemStack;
-import org.graalvm.compiler.lir.aarch64.AArch64Move;
-
-import java.util.logging.Logger;
 
 public abstract class SimpleEntity extends VirtualEntity {
 
